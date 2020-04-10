b65e179d60bad5d783a1f2d7ed210de0a3b38c1e
Ahmad Awais <me@AhmadAwais.com>
2020-04-11 02:46:34 +0500

    📦 NEW: First commit

---
+++ utils/cli.js
@@ -0,0 +1,38 @@
+const meow = require('meow');
+const chalk = require('chalk');
+const green = chalk.green;
+const yellow = chalk.yellow;
+const cyan = chalk.cyan;
+const dim = chalk.dim;
+
+module.exports = meow(
+	`
+	Usage
+	  ${green(`paddle-affiliate-link-generator`)} ${cyan(`<command>`)} ${yellow(`[--option]`)}
+
+	Commands
+	  ${cyan(`command`)}         Description
+
+	Options
+	  ${yellow(`--option`)}, ${yellow(`-o`)}         Description
+
+	Examples
+	  ${green(`paddle-affiliate-link-generator`)} ${cyan(`command`)}
+	  ${green(`paddle-affiliate-link-generator`)} ${yellow(`--option`)}
+
+	❯ You can also run command + option at once:
+	  ${green(`paddle-affiliate-link-generator`)} ${cyan(`command`)} ${yellow(`-o`)}
+`,
+	{
+		booleanDefault: undefined,
+		hardRejection: false,
+		inferType: false,
+		flags: {
+			option: {
+				type: 'boolean',
+				default: false,
+				alias: 'o'
+			}
+		}
+	}
+);
